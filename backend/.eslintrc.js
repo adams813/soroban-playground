a572213aa632b31bc896dc164e5d5874d8b224d3
Your Name <you@example.com>
2026-05-20 09:11:35 +0100

    chore(backend): enhance build process with linting, formatting, and environment setup

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,9 +1,6 @@
 module.exports = {
   root: true,
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
